0c76edceb2ba623d941ec6e91ea956782fe57a40
veno501 <veno.banovsek@gmail.com>
2021-04-03 16:27:25 +0100

    Renamed a folder

--- diary-app/asgi.py
+++ diary-app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for thought_cloud project.
+ASGI config for diary-app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thought_cloud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diary-app.settings')
 
 application = get_asgi_application()
--- diary-app/wsgi.py
+++ diary-app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for thought_cloud project.
+WSGI config for diary-app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thought_cloud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diary-app.settings')
 
 application = get_wsgi_application()
