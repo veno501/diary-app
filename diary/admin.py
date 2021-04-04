711fee79cf85380c4948086d14b06250d60ea8d4
veno501 <veno.banovsek@gmail.com>
2021-04-04 01:51:33 +0100

    Added Database and models

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Diary, Entry
+
+admin.site.register(Diary)
+admin.site.register(Entry)
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Diary(models.Model):
+    title = models.CharField(max_length=200)
+    themePreference = models.IntegerField(default=0)
+    colorPreference = models.IntegerField(default=0)
+    def __str__(self):
+        return self.title
+
+class Entry(models.Model):
+    title = models.CharField(max_length=200)
+    dateTime = models.DateTimeField('date created')
+    contentPath = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
