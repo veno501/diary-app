c32f884be74553fec76d2b86e0b7e00e158e0cf7
veno501 <veno.banovsek@gmail.com>
2021-04-09 19:10:04 +0100

    Changed README

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # diary-app
 
-My online diary application. Log into the web app, create a personal diary and fill it with anything from writing, photos or other media.
+My online diary application. 
+Log into the web app, create personal diaries and use them for your journaling!
+
+Fill entries with your writing, anything from text to images, location data or other media.
+
+![](./iconBackgroundTop.gif)
 
 ### Admin credentials
 
@@ -9,3 +14,6 @@ Username: admin, Password: admin
 ### Test user credentials
 
 Username: testuser, Password: testpassword
+
+tram Video by Christopher Schultz from Pexels
+tree Video by Oleg Magni from Pexels
--- diary/models.py
+++ diary/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-#from uuid import uuid4
-#str(uuid4())
+# UUID generation
+#   from uuid import uuid4
+#   str(uuid4())
+
+# COLOR PALETTE for themes
+#   main thumbnailing theme: theme 0 , mix-of-the-two color #BC8250
+#   background: off-white #F8EBE3 / slightly darker #EBDED5
+#   theme 0: coral orange #C47B58 , leather brown #BC9671
+#   theme 1: teal blue #3E5952 , slate gray #9EA8A7
+#   theme 2: crimson red #AF6757 , salmon pink #BC968B
 
 class Diary(models.Model):
     uuid = models.CharField(max_length=36, null=False, blank=False)
